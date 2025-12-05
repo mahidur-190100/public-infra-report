18c40b3b6b3a88bedf74638d0f4d4909e419f0ed
Mahidur Rahman <mahidur952697@gmail.com>
2025-12-05 21:23:23 +0600

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Shared/Navbar/Navbar'
+import Footer from '../Shared/Footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+        Home is this.
+        <Footer> </Footer>
+        </div>
+  )
+}
+
+export default Home
